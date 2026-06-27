dd2c7f3d9236239cd66edbbe3cc7ae1fe37507fd
julio cesar <vcjuliocesar@outlook.com>
2026-06-26 19:35:25 -0600

    fix(staging): add missing tables

--- sql/maintenance/001_truncate_staging.sql
+++ sql/maintenance/001_truncate_staging.sql
@@ -2,7 +2,10 @@ TRUNCATE TABLE
     staging.stg_customers,
     staging.stg_orders,
     staging.stg_order_items,
+    staging.stg_geolocation,
     staging.stg_products,
     staging.stg_sellers,
-    staging.stg_order_payments
+    staging.stg_order_payments,
+    staging.stg_order_reviews,
+    staging.stg_product_category_name_translation
 RESTART IDENTITY;
